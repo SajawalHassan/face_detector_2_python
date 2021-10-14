723ed21eeb8795e6d6c133b7ad07bdf58e14489c
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-14 13:34:55 +0500

    Show accuraccy

--- main.py
+++ main.py
@@ -36,6 +36,9 @@ while True:
 
             cv.rectangle(img, bbox, (0,0,0), 2) # Draw rectangle around detected face
 
+            cv.putText(img, f"Accuraccy: {int(face.score[0]*100)}", (bbox[0], bbox[1]-5),
+             cv.FONT_HERSHEY_COMPLEX, .5, (0,255,0), 1) # Showing accuraccy
+
     # Calculating fps
     cTime = time.time()
     fps = 1/(cTime - pTime)
