33f8c4ca0b19d514cf02f04f74ed199f7252f1da
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-14 13:08:49 +0500

    Show fps

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import cv2 as cv
 import mediapipe as mp
+import time
 
-# Capturing vid (cange filename to 0 if need webcam)
+# Capturing vid (change filename to 0 if need webcam)
 capture = cv.VideoCapture("videos/vid_test_smile.3gp")
+
+pTime = 0
 
 while True:
     # Reading currunt frame
-    success, frame = capture.read()
+    success, img = capture.read()
 
     # If can't read currunt frame, break loop
     if not success:
           break
 
-    cv.imshow("Video", frame)
-    key = cv.waitKey(1)
+    # Calculating fps
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    # Show fps
+    cv.putText(img, f"Fps: {int(fps)}", (10, 50), cv.FONT_HERSHEY_COMPLEX, 1, (255,0,0), 2)
+
+    cv.imshow("Video", img)
+    key = cv.waitKey(20)
 
     if key==27:
         break # If key is pressed, break loop
